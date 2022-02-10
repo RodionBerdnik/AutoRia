19f7f3b92e016a22f93540d5da54ed3e8ffe7ad3
Rodion Berdnik <kasper13082002@gmail.com>
2022-02-10 15:10:26 +0200

    improvements

--- js/app.js
+++ js/app.js
@@ -1,15 +1,71 @@
-const CARS = JSON.parse(DATA)
+const CARS = JSON.parse(DATA);
 console.log(CARS); //array of objects with car data
+const USDrate = 28.12;
+const carsList = document.getElementById('carsList');
 
-const carsList = document.getElementById('carsList')
+
+
+
+function createCarTemplate(car) {
+
+  return `<div class="ads">
+    <img src="${car.img}" alt="${car.make} ${car.model}" width="1" height="1" loading="lazy"
+        decoding="async" class="ads-img">
+    <div class="ads-information">
+        <h2>${car.make} ${car.model} (${car.year})</h2>
+        <div class="price">
+            <h3 class="price-dollars">${car.price} $</h3>
+            <h3 class="price-hryvnas">${(car.price * USDrate).toFixed(0)} грн</h3>
+        </div>
+        <div class="specificatons">
+            <h3 class="mileage"><i class="fa-solid fa-gauge-high"></i>${(car.mileage /1000).toFixed(0)} тис. км</h3>
+            <h3 class="counry"><i class="fa-solid fa-location-dot"></i>${car.country}</h3>
+            <h3 class="fuel"><i class="fa-solid fa-gas-pump"></i>${car.fuel}</h3>
+            <h3 class="transmision">${car.transmission}</h3>
+        </div>
+        <div class="vin">
+        <i class="fa-solid fa-car-mirrors"></i>
+        ${car.vin ? car.vin : 'VIN не вказано'}
+        </div>
+        
+    </div>
+</div>`;
+}
+
+function createCarsHTML(cars) {
+  return cars.map((car) => createCarTemplate(car)).join('');
+}
 
 function renderCars(cars, carsList) {
-    carsList.innerHTML = cars.map(car => `<p>${car.make}</p>`).join('')
+  const carsHTML = createCarsHTML(cars);
+  carsList.innerHTML = carsHTML;
 }
-// renderCars(CARS, carsList)
 
-//* Должна быть функция для подсчета количества предложений
-function elements__suggestions(total, hour, day){
+renderCars(CARS, carsList);
 
-}
-elements__suggestions()
+
+
+
+
+
+
+// function makeBorshch(ingredients, stove, pan) {
+    
+// }
+// const ingredients = ['meat', 'potato', 'water', 'tomatos', 'onion', 'carrot']
+// const stove = 'indesit'
+// const pan = '3l'
+
+// makeBorshch(ingredients, stove, pan)
+
+
+
+
+// если car.vin равен тру, то выдаём car.vin иначе выдаём 'VIN не вказано'
+
+
+
+// scheme Car {
+//     vin?: string,
+//     phone?: string | number,
+// }
